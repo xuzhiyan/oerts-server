1fde29ff43c688f6bd96b351133711da3d2d9996
xuzhiyan <37604467+xuzhiyan@users.noreply.github.com>
2018-04-26 03:38:32 +0800

    4.25 图片上传

--- src/main/java/com/njfu/bysj/oerts/controller/ImageController.java
+++ src/main/java/com/njfu/bysj/oerts/controller/ImageController.java
@@ -9,51 +9,46 @@
  */
 package com.njfu.bysj.oerts.controller;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.util.ClassUtils;
-import org.springframework.util.ResourceUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.njfu.bysj.oerts.entity.JsonResult;
+import com.njfu.bysj.oerts.utils.ImageUtil;
+import com.njfu.bysj.oerts.utils.JsonUtil;
 
 /**
- * @ClassName:     	PhotoController
- * @Description:	TODO
+ * @ClassName: PhotoController
+ * @Description: TODO
  *
- * @author:    		徐至彦
- * @date:        	2018年4月16日 下午10:01:27
+ * @author: 徐至彦
+ * @date: 2018年4月16日 下午10:01:27
  *
  */
 @RestController
 public class ImageController {
 
+	@Value("${images.savepath}")
+	public String Path;
+
 	@PostMapping("/userphoto")
-	public JsonResult upLoadPhoto(@RequestParam MultipartFile userPhoto, HttpServletRequest request) throws IOException {
-//		String fileType = userPhoto.getContentType(); // image/jpeg 文件类型
-//		System.out.println(fileType);
-//		String fileName = userPhoto.getOriginalFilename(); // 文件原始名字
-//		System.out.println(fileName);
-//		System.out.println(ResourceUtils.getURL("classpath:").getPath());
-//		System.out.println(ClassUtils.getDefaultClassLoader().getResource("").getPath());
-//		File file = new File("data/userphotos/111.txt");
-//		BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(file));
-//		stream.write(111111);
-//		stream.close();
+	public JsonResult upLoadPhoto(@RequestParam MultipartFile image, @RequestParam String userPhone, @RequestParam String fileName)
+			throws IOException {
+
+		ImageUtil imageUtil = new ImageUtil();
+		String savePath = Path + userPhone + "/";
 		
-		String basePath = request.getSession().getServletContext().getRealPath("/");
-		System.out.println(basePath);
-		
-		return null;
+		if (imageUtil.saveImage(image, savePath, fileName)) {
+			return JsonUtil.success();
+		} else {
+			return JsonUtil.failed("图片上传失败");
+		}
 	}
-	
+
 }
--- src/main/java/com/njfu/bysj/oerts/utils/ImageUtil.java
+++ src/main/java/com/njfu/bysj/oerts/utils/ImageUtil.java
@@ -9,83 +9,120 @@
  */
 package com.njfu.bysj.oerts.utils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
- * @ClassName:     	ImageUtil
- * @Description:	TODO
+ * @ClassName: ImageUtil
+ * @Description: TODO
  *
- * @author:    		徐至彦
- * @date:        	2018年4月16日 下午10:15:43
+ * @author: 徐至彦
+ * @date: 2018年4月16日 下午10:15:43
  *
  */
 public class ImageUtil {
 
-	public static String saveImage(MultipartFile multipartFile, String path) {
-		
-		return path;
+	// @Value("${images.savepath}")
+	// public String savePath;
+
+	public boolean saveImage(MultipartFile multipartFile, String savePath, String fileName) throws IOException {
+
+		String fileType = multipartFile.getContentType();
+
+		// 判断文件类型
+		if (fileType.equals("image/png") || fileType.equals("image/jpeg")) {
+			if (fileType.contains("png")) {
+				fileType = ".png";
+			} else {
+				fileType = ".jpg";
+			}
+		} else {
+			return false;
+		}
+
+		File targetFile = new File(savePath);
+		if (!targetFile.exists()) {
+			targetFile.mkdirs();
+		}
+
+		FileOutputStream out = new FileOutputStream(savePath + fileName + fileType);
+
+		out.write(multipartFile.getBytes());
+		out.flush();
+		out.close();
+
+		return true;
 	}
 }
 
-
-//@PutMapping("/article/img/upload")
-//public MarkDVo uploadImg(@RequestParam("editormd-image-file") MultipartFile multipartFile)  {
-//    if (multipartFile.isEmpty() || StringUtils.isBlank(multipartFile.getOriginalFilename())) {
-//       throw new BusinessException(ResultEnum.IMG_NOT_EMPTY);
-//    }
-//    String contentType = multipartFile.getContentType();
-//    if (!contentType.contains("")) {
-//        throw new BusinessException(ResultEnum.IMG_FORMAT_ERROR);
-//    }
-//    String root_fileName = multipartFile.getOriginalFilename();
-//    logger.info("上传图片:name={},type={}", root_fileName, contentType);
-//    //处理图片
-//    User currentUser = userService.getCurrentUser();
-//    //获取路径
-//    String return_path = ImageUtil.getFilePath(currentUser);
-//    String filePath = location + return_path;
-//    logger.info("图片保存路径={}", filePath);
-//    String file_name = null;
-//    try {
-//        file_name = ImageUtil.saveImg(multipartFile, filePath);
-//        MarkDVo markDVo = new MarkDVo();
-//        markDVo.setSuccess(0);
-//        if(StringUtils.isNotBlank(file_name)){
-//            markDVo.setSuccess(1);
-//            markDVo.setMessage("上传成功");
-//            markDVo.setUrl(return_path+File.separator+file_name);
-//            markDVo.setCallback(callback);
-//        }
-//        logger.info("返回值：{}",markDVo);
-//        return markDVo;
-//    } catch (IOException e) {
-//        throw new BusinessException(ResultEnum.SAVE_IMG_ERROE);
-//    }
-//}
+// @PutMapping("/article/img/upload")
+// public MarkDVo uploadImg(@RequestParam("editormd-image-file") MultipartFile
+// multipartFile) {
+// if (multipartFile.isEmpty() ||
+// StringUtils.isBlank(multipartFile.getOriginalFilename())) {
+// throw new BusinessException(ResultEnum.IMG_NOT_EMPTY);
+// }
+// String contentType = multipartFile.getContentType();
+// if (!contentType.contains("")) {
+// throw new BusinessException(ResultEnum.IMG_FORMAT_ERROR);
+// }
+// String root_fileName = multipartFile.getOriginalFilename();
+// logger.info("上传图片:name={},type={}", root_fileName, contentType);
+// //处理图片
+// User currentUser = userService.getCurrentUser();
+// //获取路径
+// String return_path = ImageUtil.getFilePath(currentUser);
+// String filePath = location + return_path;
+// logger.info("图片保存路径={}", filePath);
+// String file_name = null;
+// try {
+// file_name = ImageUtil.saveImg(multipartFile, filePath);
+// MarkDVo markDVo = new MarkDVo();
+// markDVo.setSuccess(0);
+// if(StringUtils.isNotBlank(file_name)){
+// markDVo.setSuccess(1);
+// markDVo.setMessage("上传成功");
+// markDVo.setUrl(return_path+File.separator+file_name);
+// markDVo.setCallback(callback);
+// }
+// logger.info("返回值：{}",markDVo);
+// return markDVo;
+// } catch (IOException e) {
+// throw new BusinessException(ResultEnum.SAVE_IMG_ERROE);
+// }
+// }
 //
 //
 //
-///**
+/// **
 // * 保存文件，直接以multipartFile形式
 // * @param multipartFile
 // * @param path 文件保存绝对路径
 // * @return 返回文件名
 // * @throws IOException
 // */
-//public static String saveImg(MultipartFile multipartFile,String path) throws IOException {
-//    File file = new File(path);
-//    if (!file.exists()) {
-//        file.mkdirs();
-//    }
-//    FileInputStream fileInputStream = (FileInputStream) multipartFile.getInputStream();
-//    String fileName = Constants.getUUID() + ".png";
-//    BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path + File.separator + fileName));
-//    byte[] bs = new byte[1024];
-//    int len;
-//    while ((len = fileInputStream.read(bs)) != -1) {
-//        bos.write(bs, 0, len);
-//    }
-//    bos.flush();
-//    bos.close();
-//    return fileName;
-//}
+// public static String saveImg(MultipartFile multipartFile,String path) throws
+// IOException {
+// File file = new File(path);
+// if (!file.exists()) {
+// file.mkdirs();
+// }
+// FileInputStream fileInputStream = (FileInputStream)
+// multipartFile.getInputStream();
+// String fileName = Constants.getUUID() + ".png";
+// BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path
+// + File.separator + fileName));
+// byte[] bs = new byte[1024];
+// int len;
+// while ((len = fileInputStream.read(bs)) != -1) {
+// bos.write(bs, 0, len);
+// }
+// bos.flush();
+// bos.close();
+// return fileName;
+// }
